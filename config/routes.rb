88848a242150adcd2d77fcb1ddc51b08841cea39
Saki Ebihara <sebihara729@gmail.com>
2023-07-01 17:22:55 +0900

    coded CRUD, still need to do frontend

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # read all
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+  # display task form to create new
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # read one
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # handle POST request when creating
+  post 'tasks', to: 'tasks#create'
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+  # destroy
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
